180d6c18b0c5333bbf8e9644afcae8b102e66430
Don Johnson <dj@codetestcode.io>
2014-08-01 23:38:34 -0700

    Completed White Space Demo

---
+++ src/syntax.py
@@ -0,0 +1,20 @@
+#!usr/bin/env python
+
+
+
+def A():print("Hello from A.")
+def B():print("Hello from B.")
+def C():print("Hello from C.")
+def D():print("Hello from D.")
+
+
+def main():
+	A()
+	B()
+	C()
+	D()
+
+
+
+if __name__ == '__main__':
+	main()
